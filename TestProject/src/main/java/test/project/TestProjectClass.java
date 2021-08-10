8b49e9ac7aa1bd26370d8c8bdce0044b4d5a3438
new <new@DESKTOP-VDSRR0F>
2021-08-10 21:46:32 +0530

    Test Project

---
+++ TestProject/src/main/java/test/project/TestProjectClass.java
@@ -0,0 +1,12 @@
+package test.project;
+
+import org.testng.annotations.Test;
+
+public class TestProjectClass {
+	
+	@Test
+	public void testmethod() {
+		System.out.println("I m in test");
+	}
+
+}
